0d72e6d98ab60d3813b1f4d9dd66a1fd19a8a6da
FurkanYazbahar <32523353+FurkanYazbahar@users.noreply.github.com>
2018-03-01 11:42:15 +0300

    Create Zeplin.java

---
+++ Zeplin.java
@@ -0,0 +1,14 @@
+public class Zeplin {
+    int yolcu_sayisi;
+    int manevra;
+    int al_yol;
+    
+       Zeplin(int yolcu_sayisi){
+           this.yolcu_sayisi = yolcu_sayisi;
+           this.manevra = 80 - yolcu_sayisi;
+       }
+   // public int kar_hesabi(){
+   //    return 0;
+   // }
+       
+}
