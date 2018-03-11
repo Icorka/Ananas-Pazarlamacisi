c02606be291517cc63e4af47a55b83e753fa8d8b
FurkanYazbahar <32523353+FurkanYazbahar@users.noreply.github.com>
2018-03-11 21:47:17 +0300

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -1,27 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package furkanmetin;
 
-
+/**
+ *
+ * @author Furkan
+ */
 public class Edge {
    
     private final String id;
     private final Vertex source;
     private final Vertex destination;
-    private  int weight;
-    public double egim;
+    public  int weight;
+    public double slope;
 
-    public Edge(String id, Vertex source, Vertex destination, int weight) {
+    public Edge(String id, Vertex source, Vertex destination, int weight, double slope) {
         this.id = id;
         this.source = source;
         this.destination = destination;
         this.weight = weight;
-        
+        this.slope = slope;
     }
     
     public Edge(String id, Vertex source, Vertex destination) {
         this.id = id;
         this.source = source;
         this.destination = destination;
-//        this.weight = calculateWeight(source,destination);
+
     }
 
     public String getId() {
@@ -35,36 +43,12 @@ public class Edge {
         return source;
     }
     public int getWeight() {
-        return weight;
+       return weight;
     }
-    public double calculateWeight(Vertex source,Vertex destination){
-        double weight ;
-        double lat1 = source.latitude;
-        double lat2 = destination.latitude;
-        double lon1 = source.longitude;
-        double lon2 = destination.longitude;
-        double theta = lon1 - lon2;
-	double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-	dist = Math.acos(dist);
-	dist = rad2deg(dist);
-	dist = dist * 60 * 1.1515;
-	dist = dist * 1.609344;
-		
-
-		
-        return dist;
-    }
-
-
+   
     @Override
     public String toString() {
-        return source + " " + destination;
+        return weight+"   "+slope+"   "+source + "           " + destination;
     }
-    private static double deg2rad(double deg) {
-		return (deg * Math.PI / 180.0);
-    }
-    private static double rad2deg(double rad) {
-		return (rad * 180 / Math.PI);
-    }
-
+    
 }
