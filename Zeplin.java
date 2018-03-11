2820b8f5b04876bebc8b944bd785fabe0121a8e5
FurkanYazbahar <32523353+FurkanYazbahar@users.noreply.github.com>
2018-03-11 21:46:44 +0300

    Update Zeplin.java

--- Zeplin.java
+++ Zeplin.java
@@ -1,12 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package furkanmetin;
+
 public class Zeplin {
     int yolcu_sayisi;
-    int manevra;
-    int al_yol;
+    double manevra;
+    double al_yol;
     
        Zeplin(int yolcu_sayisi){
            this.yolcu_sayisi = yolcu_sayisi;
            this.manevra = 80 - yolcu_sayisi;
        }
+       
+public String toString() {
+        return "yolcu sayisi : "+yolcu_sayisi+" manevra : "+manevra;
+//         return name;
+    }
    // public int kar_hesabi(){
    //    return 0;
    // }
